1b8aca59972b7c1d145fb6426d2aa0b719e2d832
thebrino <bm13141455@gmail.com>
2024-03-07 04:38:15 -0300

    html and css changes, started responsive

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
-window.onload = function () {
-  move();
-};
+// const noBtn2 = document.getElementById("no-btn");
 
-function move() {
+// window.onload = function () {
+//   move();
+// };
+
+// function move() {
+//   const wrapper = document.querySelector(".wrapper");
+//   const noBtn = document.getElementById("no-btn");
+//   const wrapperRect = wrapper.getBoundingClientRect();
+//   const noBtnRect = noBtn.getBoundingClientRect();
+//   const a =
+//     Math.floor(Math.random() * (wrapperRect.width - noBtnRect.width)) + 1;
+//   const b =
+//     Math.floor(Math.random() * (wrapperRect.height - noBtnRect.height)) + 1;
+
+//   noBtn.style.left = a + "px";
+//   noBtn.style.top = b + "px";
+// }
+
+const noBtn2 = document.getElementById("no-btn");
+
+noBtn2.addEventListener("click", function () {
   const wrapper = document.querySelector(".wrapper");
   const noBtn = document.getElementById("no-btn");
   const wrapperRect = wrapper.getBoundingClientRect();
@@ -12,6 +30,7 @@ function move() {
   const b =
     Math.floor(Math.random() * (wrapperRect.height - noBtnRect.height)) + 1;
 
+  noBtn.style.position = "absolute";
   noBtn.style.left = a + "px";
   noBtn.style.top = b + "px";
-}
+});
